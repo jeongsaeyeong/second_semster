142dd065072c575c1ed7a7d22972f092f3fbb0d7
jeongsaeyeong <ooooo0516@naver.com>
2025-10-13 19:09:00 +0900

    eunseo plus

---
+++ eunseo/second/test/answer.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const Addbutton = document.querySelector('.add');
+    // querySelector를 이용하여 첫 번째 숫자를 입력받는 const를 작성하세요.
+    // querySelector를 이용하여 두 번째 숫자를 입력받는 const를 작성하세요.
+    // querySelector를 이용하여 정답을 입력받는 const를 작성하세요.
+
+    const Add = (num1, num2) =>{
+        let answer = 0;
+        return answer;
+    }
+
+    // Addbutton을 눌렀을 때 Add를 호출하고 return 받은 값을 정답란에 출력하세요.
+    // 만약 두 수를 입력받았을 때 빈값이라면 alert을 이용해 '두 수를 모두 입력해주세요'라고 안내하도록 하세요.
+})
